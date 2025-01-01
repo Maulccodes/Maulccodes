37583909e9615d2a438c63c55b12248d0e7958f5
Maulccodes <41537069+Maulccodes@users.noreply.github.com>
2025-01-01 15:46:25 -0500

    boilerplate

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args){
+		
+	int number = 60;
+	System.out.print("Factors of"+number+"are:");
+	
+	for (int i = 1; i<= number; ++1) {
+		if (number % i==0)
+			System.out.print(i+"")
+		 }
+		
+	}
+}
