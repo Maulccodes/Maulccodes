5baa7e47fa4dd26469d4f7cfe63f3e6092f7a081
Maulccodes <41537069+Maulccodes@users.noreply.github.com>
2025-07-16 15:03:28 -0400

    working copy

---
+++ index.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Main{
+	
+	public static void main(String[] args) {
+		
+		String name = JOptionPane.showInputDialog("Enter your age");
+		JOptionPane.showMessageDialog(null, "Hello "+name);
+	
+}
+
+
