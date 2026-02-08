708da44745bd253a5156a052699d2d84531801be
Maulccodes <41537069+Maulccodes@users.noreply.github.com>
2026-02-07 23:02:53 -0500

    stack practice

---
+++ stacks.java
@@ -0,0 +1,19 @@
+import.java.util.Stack;
+
+public class main{
+	
+	public static void main (String[] args) {
+		
+		Stack<String> stack = new Stack<String>();
+		
+		//System.out.println(stack.empty());
+		
+		stack.push("WOW");
+		stack.push("Apex");
+		stack.push("COD");
+		
+		stack.pop();
+		
+		System.out.println(stack);
+	}
+}
