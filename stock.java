231f333ef69b89440642e41ca7ffcbca56857e93
Maulccodes <41537069+Maulccodes@users.noreply.github.com>
2026-02-07 22:41:32 -0500

    stock for java

---
+++ stock.java
@@ -0,0 +1,12 @@
+
+public class Stock 
+
+	public static void main(String[] args)
+	
+	private string symbol;
+	private string name;
+	private double priorCurrentPrice;
+	private double currentPrice;
+	
+	while(symbol == 0)
+	
